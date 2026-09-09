cc32f619b0c68ad61f94f5570dad26e447ccd847
Oscar <opinto@tdw.group>
2026-09-09 15:13:35 -0600

    Agregando tabla de la capa gold

---
+++ dbt_demo/models/Gold/resumen_ordenes.sql
@@ -0,0 +1,8 @@
+{{config(materialized='table')}}
+
+select fechacarga-o_orderdate as dias_transcurridos, 
+o_orderpriority, 
+count(*) as cantidad_ordenes
+from DBT_TEST.DBT_OSCAR.ORDER_COMPLETAS_SILVERL
+group by 1,2
+
