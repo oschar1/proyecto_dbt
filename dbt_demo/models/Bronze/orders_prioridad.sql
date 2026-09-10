f3dd48c1f6eb3c9a6f9637ff50172ce200486f68
Oscar <opinto@tdw.group>
2026-09-10 10:27:02 -0600

    Agreando un modelo trabjando con jinja

---
+++ dbt_demo/models/Bronze/orders_prioridad.sql
@@ -0,0 +1,19 @@
+
+{{ config(materialized='table') }}
+{% set prioridades = var('prioridades') %}
+
+SELECT
+    O_ORDERDATE,
+    {% for prioridad in prioridades %}
+    sum(CASE WHEN O_ORDERPRIORITY = '{{ prioridad }}' THEN 1 ELSE 0 END) AS "{{ prioridad }}",
+    {% endfor %}
+    count(O_ORDERPRIORITY) AS total_
+FROM
+    SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.ORDERS
+GROUP BY
+    O_ORDERDATE
+
+
+    /** el llamado se realizo asi:
+    dbt run --select orders_prioridad --vars "prioridades: ['2-HIGH', '3-MEDIUM', '1-URGENT', '5-LOW', '4-NOT SPECIFIED']"
+    /
